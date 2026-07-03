4576c3efe5e314f78a7d5db04f2a4d4e64e9e826
pranavkalladapk-art <pranavkallada.pk@gmail.com>
2026-07-03 11:09:14 +0530

    Add RoofScene component for 3D rendering

---
+++ src/components/three/RoofScene.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Lightformer, Sparkles } from "@react-three/drei";
+import RoofStructure from "./RoofStructure";
+import { useIsMobile } from "@/lib/useIsMobile";
+
+export default function RoofScene({
+  progressRef,
+}: {
+  progressRef: React.MutableRefObject<number>;
+}) {
+  const isMobile = useIsMobile();
+
+  return (
+    <Canvas
+      dpr={isMobile ? [1, 1.25] : [1, 1.75]}
+      gl={{ antialias: !isMobile, powerPreference: "high-performance" }}
+      camera={{ position: [2, 1, 11], fov: 34 }}
+      shadows={!isMobile}
+    >
+      <color attach="background" args={["#050505"]} />
+      <fog attach="fog" args={["#050505", 12, 22]} />
+
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[4, 6, 5]} intensity={1.6} castShadow={!isMobile} />
+      <pointLight position={[-5, -1, -3]} intensity={7} color="#00d5ff" distance={14} />
+      <pointLight position={[4, -2, 3]} intensity={4} color="#5be8ff" distance={12} />
+
+      <Suspense fallback={null}>
+        <RoofStructure progressRef={progressRef} />
+        <Sparkles count={isMobile ? 20 : 50} scale={[9, 8, 6]} size={1.5} speed={0.2} opacity={0.35} color="#5be8ff" />
+        <Environment resolution={256}>
+          <group>
+            <Lightformer intensity={2} color="#ffffff" position={[0, 4, -5]} scale={[10, 2, 1]} form="rect" />
+            <Lightformer intensity={4} color="#00d5ff" position={[-6, 0, 2]} scale={[1, 8, 1]} form="rect" />
+            <Lightformer intensity={2.5} color="#5be8ff" position={[6, -1, 3]} scale={[1, 6, 1]} form="rect" />
+          </group>
+        </Environment>
+      </Suspense>
+    </Canvas>
+  );
+}
